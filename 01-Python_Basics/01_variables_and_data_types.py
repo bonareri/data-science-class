b157d6ff7b354d97c9f39005b44c20619d21ecce
bonareri <melodybonareri123@gmail.com>
2025-04-13 20:18:00 +0300

    Updated the variables and data types file

---
+++ 01-Python_Basics/01_variables_and_data_types.py
@@ -0,0 +1,31 @@
+'''
+Variables 
+A variable is a name used to store data in memory
+You can assign a value using the = operator 
+'''
+
+name = "Melody"
+age = 25
+print(name)
+
+'''
+#Rules for naming variables
+Must start with a letter or underscore_
+Cannot start with a number
+Case sensitive(Age and age are different)
+Cannot use python keyword(e.g "If", "Class", "for")
+'''
+
+'''
+Common Data Types
+Str - "Hello" - String(Text)
+int - 42 - Integer(Whole number)
+float - 3.14 - Float(Decimal)
+bool - True, False - Boolean
+list -  [1,2,3,] - Ordered, Changeable collection, 
+set - {1,2,3} - Unorderd, unique items
+dict - {"key" : "value"} - key-value pairs  
+'''
+
+#Use type() to check the data type of a variable
+print(type(name))
